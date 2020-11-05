5625aa69292a4086722e13b1081c7a136478bc1c
joonlee7 <joonplee7@gmail.com>
2020-11-05 11:47:18 -0500

    Angular

--- Angular_Project/angular-forms/src/app/fakeAPI/fake.component.ts
+++ Angular_Project/angular-forms/src/app/fakeAPI/fake.component.ts
@@ -14,6 +14,7 @@ export class FakeComponent implements OnInit {
   msg2: string;
 
   fakeObj: Fake[];
+  employeeDetails: Employee[] = [];
 
   constructor(private fakeSer2: FakeService2) { }
 
@@ -26,8 +27,32 @@ export class FakeComponent implements OnInit {
     this.msg2 = this.fakeSer2.sayHello();
   }
 
-  ngOnInit(): void {
-    this.fakeSer2.loadData().subscribe(data=>this.fakeObj=data);
+  i = 0;
+  id = 0;
+  name = null;
+  salary = 0.0;
+  loadEmpDetails() {
+    this.id = this.employeeDetails[this.i].id;
+    this.name = this.employeeDetails[this.i].name;
+    this.salary = this.employeeDetails[this.i].salary;
+    this.i++;
   }
 
+  ngOnInit(): void {
+    // this.fakeSer2.loadData().subscribe(data=>this.fakeObj=data);
+    // this.fakeSer2.loadData().subscribe(data => console.log(data,
+    //   (error: any) => console.log(error),
+    //   () => console.log('Completed')));
+    this.fakeSer2.loadData().subscribe(result => this.fakeObj = result);
+    this.employeeDetails.push(new Employee(100, "Joon", 70000));
+    this.employeeDetails.push(new Employee(101, "Bill", 45000));
+    this.employeeDetails.push(new Employee(102, "Sally", 50000));
+    this.employeeDetails.push(new Employee(101, "Sandi", 7));
+  }
+}
+
+export class Employee {
+  constructor(public id: number, public name: string, public salary: number) {
+
+  }
 }
